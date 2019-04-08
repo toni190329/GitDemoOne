3a3ab4bb1c35764f7c1a1380401882421e6ca802
toni190329 <tonizb21@126.com>
2019-04-08 15:55:19 +0800

    第4次提交到本地仓库！

--- src/com/jbit/test/T.java
+++ src/com/jbit/test/T.java
@@ -5,8 +5,5 @@ public class T {
         System.out.println("hello!");
         System.out.println("hello!");
         System.out.println("hello1");
-        System.out.println("hello2");
-        System.out.println("hello3");
-    }
     }
 }
